c85dce0d1a7e7dcad3b5024f2eb1f745bb1ca93c
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-05-12 12:42:56 +0300

    refactor: update apiUrl configuration for development and production environments

--- mobile/src/config/app.config.ts
+++ mobile/src/config/app.config.ts
@@ -2,7 +2,12 @@ import Constants from "expo-constants";
 
 const debuggerHost = Constants.expoConfig?.hostUri?.split(":")[0];
 
+const isDev = __DEV__;
+
 export const AppConfig = {
-  apiUrl: `http://${debuggerHost}:3001`,
+  apiUrl:
+    isDev && debuggerHost
+      ? `http://${debuggerHost}:3001`
+      : "https://fit-life-api.netlify.app",
   apiTimeout: 35000,
 } as const;
