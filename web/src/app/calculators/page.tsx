587999b87301f5afc4496bcb25c6bc736e0f235f
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-05-28 18:21:31 +0300

    feat: add API routes for managing workouts and user challenges with authentication and CRUD operations

---
+++ web/src/app/calculators/page.tsx
@@ -0,0 +1,13 @@
+﻿"use client";
+
+import PrivateRoute from "@/src/components/PrivateRoute";
+import Calculators from "@/src/views/Calculators/Calculators";
+
+export default function CalculatorsPage() {
+  return (
+    <PrivateRoute>
+      <Calculators />
+    </PrivateRoute>
+  );
+}
+
