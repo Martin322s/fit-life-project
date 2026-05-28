678c50cb115a5126652d4e2767102ac85c9ca462
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-05-28 18:22:48 +0300

    feat: add login, products, profile, recipes, and register pages with appropriate routing

---
+++ web/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+﻿"use client";
+
+import GuestRoute from "@/src/components/GuestRoute";
+import Login from "@/src/views/Login/Login";
+
+export default function LoginPage() {
+  return (
+    <GuestRoute>
+      <Login />
+    </GuestRoute>
+  );
+}
+
